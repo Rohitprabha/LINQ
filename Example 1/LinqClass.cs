31d31af7d9070e3c3da8bdc1e24f7b6aca3d863d
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-04 20:18:52 +0530

    Add files via upload

---
+++ Example 1/LinqClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Example_1
+{
+    class LinqClass
+    {
+        static void Main()
+        {
+            int[] a = { 100, 30, 20, 40, 80, 50, 10, 60, 70, 90 };
+            var b = from i in a where i >= 50 orderby i descending select i;
+            foreach (int x in b)
+                Console.Write(x + " ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LINQ_Example_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 100, 30, 20, 40, 80, 50, 10, 60, 70, 90 };
+            int count = 0;
+            foreach (int x in a)
+            {
+                if (x >= 50)
+                    count += 1;
+            }
+            int[] b = new int[count];
+            int index = 0;
+            foreach (int x in a)
+            {
+                if (x >= 50)
+                {
+                    b[index] = x;
+                    index++;
+                }
+            }
+            Array.Sort(b);
+            Array.Reverse(b);
+            foreach (int x in b)
+                Console.Write(x + " ");
+            Console.ReadLine();
+        }
+    }
+}
